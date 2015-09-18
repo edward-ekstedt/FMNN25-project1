a33315cc9c40bc89ee50c03fc20d210eeb7faeca
Johannes Olsson <joholss@gmail.com>
2015-09-18 16:10:18 +0200

    Fix
    Small fix

--- Bspline.py
+++ Bspline.py
@@ -17,30 +17,32 @@ class spline(object):
         uy = u[1]
         xix = (ux[:-2]+ux[1:-1]+ux[2:])/3.
         xiy = (uy[:-2]+uy[1:-1]+uy[2:])/3.
-        self.xi = np.vstack((xix,xiy))        
+        self.xi = np.vstack((xix,xiy))    
+        
     def ma_array(self):
         u = self.u
         self.xi = (u[:-2]+u[1:-1]+u[2:])/3.
-        self.xi = np.vstack((xix,xiy))  
+        self.xi = np.vstack((self.xix,self.xiy))  
+        
     def computeNXi(self, u, k, i, xi):
-    if k==0:
-        if u[i-1] == u[i]:
-            return 0
-        elif u[i]>xi>=u[i-1]:
-            return 1
+        if k==0:
+            if u[i-1] == u[i]:
+                return 0
+            elif u[i]>xi>=u[i-1]:
+                return 1
+            else:
+                return 0
         else:
-            return 0
-    else:
-        if u[i-1]==u[i+k-1]:
-            coef1=0
-        else:
-            coef1=(xi-u[i-1])/(u[i+k-1]-u[i-1])
-        if u[i+k]==u[i]:
-            coef2=0
-        else:
-            coef2=(u[i+k]-xi)/(u[i+k]-u[i])
-        NXi = coef1*computeNXi(u, k-1, i, xi)+coef2*computeNXi(u, k-1, i+1, xi)
-        return NXi
+            if u[i-1]==u[i+k-1]:
+                coef1=0
+            else:
+                coef1=(xi-u[i-1])/(u[i+k-1]-u[i-1])
+            if u[i+k]==u[i]:
+                coef2=0
+            else:
+                coef2=(u[i+k]-xi)/(u[i+k]-u[i])
+            NXi = coef1*self.computeNXi(u, k-1, i, xi)+coef2*self.computeNXi(u, k-1, i+1, xi)
+            return NXi
 
         
         

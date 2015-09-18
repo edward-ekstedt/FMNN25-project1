fec6d7e67b77b3f048136908c9a91d7ea7c87e97
edwardekstedt <edwardekstedt@gmail.com>
2015-09-18 13:02:23 +0200

    indentation
    changed indentation errors

--- Bspline.py
+++ Bspline.py
@@ -23,22 +23,22 @@ class spline(object):
         self.xi = (u[:-2]+u[1:-1]+u[2:])/3.
         self.xi = np.vstack((xix,xiy))  
     def computeNXi(self, u, k, i, xi):
-    if k==0:
-        if u[i-1] == u[i]:
-            return 0
-        elif u[i]>xi>=u[i-1]:
-            return 1
+        if k==0:
+            if u[i-1] == u[i]:
+                return 0
+            elif u[i]>xi>=u[i-1]:
+                return 1
+            else:
+                return 0
         else:
-            return 0
-    else:
-        if u[i-1]==u[i+k-1]:
-            coef1=0
-        else:
-            coef1=(xi-u[i-1])/(u[i+k-1]-u[i-1])
-        if u[i+k]==u[i]:
-            coef2=0
-        else:
-            coef2=(u[i+k]-xi)/(u[i+k]-u[i])
+            if u[i-1]==u[i+k-1]:
+                coef1=0
+            else:
+                coef1=(xi-u[i-1])/(u[i+k-1]-u[i-1])
+            if u[i+k]==u[i]:
+                coef2=0
+            else:
+                coef2=(u[i+k]-xi)/(u[i+k]-u[i])
         NXi = coef1*computeNXi(u, k-1, i, xi)+coef2*computeNXi(u, k-1, i+1, xi)
         return NXi
 

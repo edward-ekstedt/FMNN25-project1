627c47ae24ca0246ef97dfb85aadcfb778ae92e2
edwardekstedt <edwardekstedt@gmail.com>
2015-09-18 12:52:48 +0200

    one dimension
    added support for one dimension

--- Bspline.py
+++ Bspline.py
@@ -10,20 +10,18 @@ import scipy as sp
 
 class spline(object):
     def ma_matrix(self):
+        # Computes the moving average of a 2-dimensional matrix over 3 elements
+        # Creates the vector xi_i
         u = self.u
         ux = u[0]
         uy = u[1]
         xix = (ux[:-2]+ux[1:-1]+ux[2:])/3.
         xiy = (uy[:-2]+uy[1:-1]+uy[2:])/3.
         self.xi = np.vstack((xix,xiy))        
+    def ma_array(self):
+        u = self.u
+        self.xi = (u[:-2]+u[1:-1]+u[2:])/3.
         
-    
-
-u = np.array([[0,0,0,1,2,2,2],[1,1,1,2,3,3,3]])
-ux = u[0]
-uy = u[1]
-xix = (ux[:-2]+ux[1:-1]+ux[2:])/3.
-xiy = (uy[:-2]+uy[1:-1]+uy[2:])/3.
-xi = np.vstack((xix,xiy))       
-print(xi)
-
+        
+        
+        

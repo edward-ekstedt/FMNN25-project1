c7e5db4f1677f1677ad5ecd930596b265e34c7ae
edwardekstedt <edwardekstedt@gmail.com>
2015-09-21 16:13:55 +0200

    Plotting enabled
    Fungerar för kurvdesign, går ej med plot

--- Bspline.py
+++ Bspline.py
@@ -6,7 +6,7 @@ Created on Fri Sep 18 09:35:16 2015
 """
 import numpy as np
 import scipy as sp
-
+import matplotlib.pyplot as plt
 
 class spline(object):
     
@@ -16,22 +16,27 @@ class spline(object):
         self.control = control
         
     def __call__(self,u):
-        s = self.computeS(u)
-        
+        self.s = self.computeS(u)
 
 
     def computeS(self,u):
-        index = self.findHot(u)
-        dx = self.sU(u,index,index+1,0)
-        dy = self.sU(u,index,index+1,1)
-        return np.array[[dx],[dy]]
+        sX = np.zeros((len(u)))
+        sY = np.zeros((len(u)))
+        for i in range(0,len(u)):
+            index = self.findHot(u[i])-1
+            sX[i] = self.sU(u[i],index,index+1,0)
+            sY[i] = self.sU(u[i],index,index+1,1)
+        return np.array([[sX],[sY]])
         
     def sU(self,u,rightMost,leftMost,dim):
         #Calculates the blossoms with recursive algorithm
         if rightMost - leftMost == 2:
-            return self.control[dim,leftMost]
+            return self.control[dim,leftMost-1]
+        elif self.knots[rightMost]-self.knots[leftMost] == 0:
+            alpha = 0 #(self.knots[rightMost]-u)
+            return alpha*self.sU(u,rightMost,leftMost-1,dim)+(1-alpha)*self.sU(u,rightMost+1,leftMost,dim)
         else:
-            alpha = (self.knots[rightMost]-u)/(self.knots[rightMost]-self.knots[leftMost])
+            alpha = (self.knots[rightMost+1]-u)/(self.knots[rightMost+1]-self.knots[leftMost-1])
             return alpha*self.sU(u,rightMost,leftMost-1,dim)+(1-alpha)*self.sU(u,rightMost+1,leftMost,dim)
     def findHot(self,u):
         #jomp
@@ -70,7 +75,17 @@ class spline(object):
         return NXi
 
 
-        
-        
-        
+    def plot(self):
+        plt.figure(1)
+        plt.plot(self.s[0],self.s[1],'bo')
+        plt.plot(self.control[0],self.control[1],'ro')
+        plt.plot(self.control[0],self.control[1],'r--')
+        plt.show()
+knots = np.linspace(0,1,12)
+u = np.linspace(0,1,100)
+knots = np.hstack(([0,0],knots,[1, 1]))
+control = np.array([[0.,1,2,3,2,1,-1,-2,-3,-4,-5,-6,-7],[0.,3,2,1,-2,-3,-2,-1,2,3,4,5,6]])
+Sp = spline(knots,control)
+Sp(u)
+Sp.plot()
         
